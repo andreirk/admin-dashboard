cde151debbbe00c4edd47bbd7dfdcd3b98898a51
Andrey Sokolov <sokolik_85@list.ru>
2016-12-06 09:55:54 +0700

    change docker image name target

--- circleci/circle_aws_deploy.sh
+++ circleci/circle_aws_deploy.sh
@@ -9,9 +9,9 @@ export UIADMINV2_HASH=$(git rev-parse HEAD)
 
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker build -t arammeem16/toyou-ui-admin:${UIADMINV2_HASH} .
-docker push docker.io/arammeem16/toyou-ui-admin:${UIADMINV2_HASH}  
+docker build -t arammeem16/toyou-ui-admin-v2:${UIADMINV2_HASH} .
+docker push docker.io/arammeem16/toyou-ui-admin-v2:${UIADMINV2_HASH}  
 
 printf '{"UIADMINV2_HASH" : "%s"}\n' ${UIADMINV2_HASH} >/tmp/${UIADMINV2_HASH}.json
-aws --region eu-central-1 s3 cp /tmp/${UIADMINV2_HASH}.json s3://arm-conf/${TARGET_STACK}/versions/uiadmin-apps.json
+aws --region eu-central-1 s3 cp /tmp/${UIADMINV2_HASH}.json s3://arm-conf/${TARGET_STACK}/versions/uiadmin-apps-v2.json
 ssh ubuntu@${TARGET_HOST} -t "cd backend-infrastructure && bash -lc './run-cox-update ${TARGET_STACK}'" || true # always return success
