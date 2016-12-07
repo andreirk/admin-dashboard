8421d11bc25740e91fa43082976ddf4876474842
Andrey Sokolov <sokolik_85@list.ru>
2016-12-07 10:45:51 +0700

    change rebuild & nginx config

--- rebuild.sh
+++ rebuild.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-docker stop ui-admin-v2
-docker rm ui-admin-v2
-docker rmi ui-admin-v2
+docker stop dashboard
+docker rm dashboard
+docker rmi dashboard
 
 #docker build -t arammeem16/toyou-ui-admin:9da6c1ce9c09bb375f7b4ef982151ae39aac2cb8 .
 #docker run -d --name=ui-admin arammeem16/toyou-ui-admin
 
-docker build -f Dockerfile-develop -t ui-admin-v2 .
-docker run -d -p 8090:8090 --name=ui-admin-v2 -v ${PWD}/dist/:/usr/share/nginx/html/ui-admin-v2/ ui-admin-v2
+docker build -f Dockerfile-develop -t dashboard .
+docker run -d -p 8090:8090 --name=dashboard -v ${PWD}/dist/:/usr/share/nginx/html/dashboard/ dashboard
 
