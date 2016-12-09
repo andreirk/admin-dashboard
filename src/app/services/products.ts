fb296cca01ab3f1aa53d8052ac9605aab01d6e8f
Andrey Sokolov <sokolik_85@list.ru>
2016-12-09 10:16:54 +0700

    add api  service and product api

---
+++ src/app/services/products.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core'
+import { ApiService } from './api'
+
+@Injectable()
+export class ProductService {
+    path: string = '/merchants';
+
+    constructor(private api: ApiService){
+          
+    }
+
+    createProduct(product) {
+        return this.api.post(this.path, product)
+    }
+
+    getProducts(){
+        return this.api.get(this.path)
+    }
+
+    completeProduct(product) {
+        return this.api.delete(`${this.path}/${product.id}`)
+    }
+}
