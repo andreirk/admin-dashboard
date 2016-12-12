7c3256c8be66946a3da0da6fc504c55529ffe12f
Andrey Sokolov <sokolik_85@list.ru>
2016-12-12 16:42:01 +0700

    change aws

--- circleci/circle_aws_deploy.sh
+++ circleci/circle_aws_deploy.sh
@@ -21,15 +21,17 @@ echo Hello from ls
 ls
 
 
+
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 docker build -t arammeem16/toyou-dashboard:${UIADMINV2_HASH} .
 docker push docker.io/arammeem16/toyou-dashboard:${UIADMINV2_HASH}  
 
 echo Hello from ls dist
+ls dist
 
 
 printf '{"UIADMINV2_HASH" : "%s"}\n' ${UIADMINV2_HASH} >/tmp/${UIADMINV2_HASH}.json
-aws --region eu-central-1 s3 cp /tmp/${UIADMINV2_HASH}.json s3://arm-conf/${TARGET_STACK}/versions/uiadmin-apps-v2.json
+aws --region eu-central-1 s3 cp /tmp/${UIADMINV2_HASH}.json s3://arm-conf/${TARGET_STACK}/versions/uiadmin-apps.json
 echo Hello after aws
 
 ssh ubuntu@${TARGET_HOST} -t "cd backend-infrastructure && bash -lc './run-cox-update ${TARGET_STACK}'" || true # always return success
