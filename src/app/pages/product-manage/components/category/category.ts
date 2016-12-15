560fd45f47a203611bfdc9429169cb16a42648e3
Andrey Sokolov <sokolik_85@list.ru>
2016-12-15 19:18:35 +0700

    category list & crud + image upload

---
+++ src/app/pages/product-manage/components/category/category.ts
@@ -0,0 +1,17 @@
+export class Category {
+
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    public content?: string,
+    public alterEgo?: string  
+    // public description: string,
+    // public imageUrl: string,
+    // public name: string,
+    // public sectionType?: string,
+    // public sortOrder?: number,
+    // public type?: string
+  ) {  }
+
+}
