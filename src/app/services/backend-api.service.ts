6817f802f09e0b32b0abdbc03f25529b7efec55f
Andrey Sokolov <sokolik_85@list.ru>
2016-12-19 19:01:53 +0700

    pull files from mearckent branch

---
+++ src/app/services/backend-api.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, Response, URLSearchParams, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import 'rxjs/add/observable/throw';
+
+@Injectable()
+export class BackendApiService {
+  headers: Headers = new Headers({
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': 'Basic YWRtaW46MDAwMA=='
+  });
+
+  constructor(private http: Http) {
+  }
+
+  get(path: string, params: Object, lang: string): Observable<any> {
+    this.headers.set('Accept-Language', lang);
+    console.log('in backend api get lang', lang)
+
+    return this.http.get(path, this.getRequestOptions(params, lang))
+      .map(this.checkForError)
+      .catch(err => Observable.throw(err))
+      .map(this.getJson)
+  }
+
+  post(path: string, body: Object, params: Object, lang: string): Observable<any> {
+    console.log('in backend-api.service.ts post leng', lang  )
+    return this.http.post(path, body, this.getRequestOptions(params, lang))
+      .map(this.checkForError)
+      .catch(err => Observable.throw(err))
+      .map(this.getJson)
+  }
+
+  put(path: string, body: Object, params: Object, lang: string): Observable<any> {
+    return this.http.put(path, body, this.getRequestOptions(params, lang))
+      .map(this.checkForError)
+      .catch(err => Observable.throw(err))
+      .map(this.getJson)
+  }
+
+  delete(path: string): Observable<any> {
+    return this.http.delete(path, this.headers)
+      .map(this.checkForError)
+      .catch(err => Observable.throw(err))
+      .map(this.getJson)
+  }
+
+  private getJson(resp: Response){
+    return resp.json();
+  }
+
+  private checkForError(resp: Response): Response{
+    if(resp.status >= 200 && resp.status < 300){
+      return resp;
+    } else {
+      const error = new Error(resp.statusText);
+      error['response'] = resp;
+      console.log(error);
+      throw error;
+    }
+  }
+
+  private getRequestOptions(params: Object, lang: string): RequestOptions {
+    console.log('in getRequest option lang', lang)
+    this.headers.set('Accept-Language', lang);
+
+    let urlparams = new URLSearchParams();
+    Object.keys(params).forEach(key => urlparams.set(key, params[key]));
+    return new RequestOptions({
+      headers: this.headers,
+      search: urlparams
+    });
+  }
+}
+
