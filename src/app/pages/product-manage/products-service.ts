b11d109d5e916f5be858193529fe1f7d21c411db
Andrey Sokolov <sokolik_85@list.ru>
2016-12-19 19:04:14 +0700

    change products for new api

--- src/app/pages/product-manage/products-service.ts
+++ src/app/pages/product-manage/products-service.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@angular/core'
-import { ApiService } from '../../services/api'
+import { BackendApiService as ApiService } from '../../services/backend-api.service'
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Injectable()
 export class ProductService {
-    path: string = '/merchants'; // no products yet
+    path: string = '/catalog/mgmt/v1/categories'; // no products yet
 
     constructor(private api: ApiService){
     }
+
+    lang = 'en'
 
     createProduct(product) {
         let headers = new Headers({ 
         'Content-Type': 'application/json'             
         });
-        return this.api.post(this.path, product, headers )
+        return this.api.post(this.path, product, {}, this.lang )
     }
 
     getProducts(){
-        return this.api.get(this.path)
+        return this.api.get(this.path,{}, this.lang)
     }
 
     deleteProduct(product) {
