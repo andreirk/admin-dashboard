83240a349d584fc323d721b5b74f7aead86762db
Andrey Sokolov <sokolik_85@list.ru>
2016-12-20 15:54:10 +0700

    add string

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,6 +3,7 @@ const path = require('path');
 const webpackMerge = require('webpack-merge'); // used to merge webpack configs
 const commonConfig = require('./webpack.common.js'); // the settings that are common to prod and dev
 
+
 /**
  * Webpack Plugins
  */
