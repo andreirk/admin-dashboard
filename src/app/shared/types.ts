a6941e2e90fd7618e146aca5175b409f7c3696bd
dbabiy <dbabiy@setronica.com>
2016-12-21 10:59:31 +0700

    BACK-736 merchant management

---
+++ src/app/shared/types.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright © 2016 Aram Meem Company Limited.  All Rights Reserved.
+ */
+
+export enum SectionType {
+  deals = <any>'deals',
+  catalogs = <any>'catalogs',
+  stores = <any>'stores',
+  specials = <any>'specials'
+}
+
+export enum RootCategoryType {
+  REGULAR = <any>'REGULAR',
+  MEAL = <any>'MEAL',
+  GIFT = <any>'GIFT',
+  PROMO = <any>'PROMO',
+  TRENDING = <any>'TRENDING',
+  FEAT_CATEGORIES = <any>'FEAT_CATEGORIES',
+  FEAT_PRODUCTS = <any>'FEAT_PRODUCTS'
+}
