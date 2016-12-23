05cac4709f741e2009bc0d0318a4d71858ca15cd
Andrey Sokolov <sokolik_85@list.ru>
2016-12-23 19:15:43 +0700

    BACK-718 service get data interval

--- src/app/pages/maps/components/driverMap/driverMap.service.ts
+++ src/app/pages/maps/components/driverMap/driverMap.service.ts
@@ -1,35 +1,27 @@
 import { Injectable } from '@angular/core';
 import { BackendApiService  } from './../../../../services/backend-api.service';
-import { Headers, Http, Response, URLSearchParams, RequestOptions } from '@angular/http';
 
 import {Observable} from 'rxjs/Rx';
 
 @Injectable()
 export class DriverMapService {
-    private path = '/driver/mgmt/v1/driverlocation';
-    headers: Headers = new Headers({
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization': 'Basic YWRtaW46MDAwMA=='
-  }); 
-    options = {
-        headers : this.headers
-    }
-    constructor(private api: BackendApiService, private http: Http){
+  private path = '/driver/mgmt/v1/driverlocation';
 
-    }
-    
-    counter() {
+  constructor(private api: BackendApiService) {
+
+  }
+
+  getLocationsByInterval(interval) {
     return Observable
-        .interval(2000)
-        .flatMap(() => {
-            return  this.http.get(this.path, this.options)
-        });
-}
+      .interval(interval)
+      .flatMap(() => {
+        return this.api.get(this.path, {}, 'en')
+      });
+  }
 
-    getDriverLocations(){
-        return this.http.get(this.path, this.options)
-    }
+  getDriverLocations() {
+    return this.api.get(this.path, {}, 'en')
+  }
 
 
 }
