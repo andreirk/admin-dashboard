07f87d853f646faf455b7c3af55029105d7c482d
Andrey Sokolov <sokolik_85@list.ru>
2016-12-26 18:14:39 +0700

    remove unused ui components & moved dirivers to its own module

---
+++ src/app/pages/drivers/components/driverMap/driverMap.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { BackendApiService  } from '../../../../core/services/backend-api.service';
+
+import {Observable} from 'rxjs/Rx';
+
+@Injectable()
+export class DriverMapService {
+  private path = '/driver/mgmt/v1/driverlocation';
+
+  constructor(private api: BackendApiService) {
+
+  }
+
+  getLocationsByInterval(interval) {
+    return Observable
+      .interval(interval)
+      .flatMap(() => {
+        return this.api.get(this.path, {}, 'en')
+      });
+  }
+
+  getDriverLocations() {
+    return this.api.get(this.path, {}, 'en')
+  }
+
+
+}
