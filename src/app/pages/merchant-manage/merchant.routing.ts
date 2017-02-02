1cf121572e50951cae751ca1e2888f8d93cc6a0e
Andrey Sokolov <sokolik_85@list.ru>
2017-02-02 15:58:35 +0700

    BACK-783 format imports

--- src/app/pages/merchant-manage/merchant.routing.ts
+++ src/app/pages/merchant-manage/merchant.routing.ts
@@ -4,12 +4,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { MerchantListComponent } from './components/mrechant-list/merchant-list.component';
 import { MerchantDetailsComponent } from './components/merchant-details/merchant-details.component';
-import {PosListComponent} from "./components/pos/pos-list.component";
-import {PosDetailsComponent} from "./components/pos/pos-details/pos-details.component";
-import {MerchantSectionsComponent} from "./components/merchant-sections.component";
-import { CanDeactivateGuard } from "../../core/services/guards/can-deactivate-guard.service";
-import { ProductListContainerComponent } from "./components/products/product-list-container.component";
-import { ProductDetailsContainerComponent } from "./components/products/product-details-container";
+import { PosListComponent } from './components/pos/pos-list.component';
+import { PosDetailsComponent } from './components/pos/pos-details/pos-details.component';
+import { MerchantSectionsComponent } from './components/merchant-sections.component';
+import { CanDeactivateGuard } from '../../core/services/guards/can-deactivate-guard.service';
+import { ProductListContainerComponent } from './components/products/product-list-container.component';
+import { ProductDetailsContainerComponent } from './components/products/product-details-container';
 import { ProductOptionListContainerComponent } from './components/product-options/components/product-options-list-contaiter.component';
 import { ProductOptionDetailsContainerComponent } from './components/product-options/components/product-option-details-container.component';
 
